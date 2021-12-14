b1652b656e069c3a60cf23a93775204ccc77a7d4
mattlax <48673695+mattlax@users.noreply.github.com>
2021-12-14 13:23:03 +0100

    Add files via upload

---
+++ genepi_site.js
@@ -0,0 +1,65 @@
+var map = L.map('map', {
+    minZoom: 7,
+});
+var osmUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
+var osmAttrib = 'Map data © OpenStreetMap contributors';
+var osm = new L.TileLayer(osmUrl, { attribution: osmAttrib }).addTo(map);
+map.setView([45.733, 4.832], 7);
+
+
+
+////////////////////////////////////////////////////
+//////////////// ajout de la couche region ////////////////
+
+function styleREG_ARA(feature) {
+    return {
+        fillColor: 'red',
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.5
+    }
+};
+
+var regionARA = L.geoJSON(region_ARA, {
+    pointToLayer: function (feature, latlng) {
+        return L.polygon(latlng, { icon: icon });
+    },
+    style: styleREG_ARA
+}).addTo(map);
+
+/////////////////////////////////////////////////////
+//////////////// Couche departement ////////////////
+/////////////////////////////////////////////////////
+function styleDEP_ARA(feature) {
+    return {
+        fillColor: 'blue',
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '3',
+        fillOpacity: 0.2
+    }
+};
+
+var depARA = L.geoJSON(dep_ARA, {
+    pointToLayer: function (feature, latlng) {
+        return L.polygon(latlng, { icon: icon });
+    },
+    style: styleDEP_ARA
+}).addTo(map);
+
+
+/////////////////////////////////////////////////////
+//////////////// panneau de contrôle ////////////////
+/////////////////////////////////////////////////////
+
+var mixed = {
+    "région": regionARA, // BaseMaps
+    "departement": depARA, 		// BaseMaps
+    
+    
+};
+
+L.control.layers(null, mixed).addTo(map);
